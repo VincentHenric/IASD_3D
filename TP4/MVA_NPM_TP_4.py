c31f8657daccb1d041d3a64d2d965ddb1796f2b0
HENRIC <vincent.henric@hotmail.fr>
2020-02-09 14:35:10 +0100

    change scalar product

--- TP4/MVA_NPM_TP_4.py
+++ TP4/MVA_NPM_TP_4.py
@@ -18,11 +18,16 @@ import matplotlib.pyplot as plt
 
 class Material:
     def __init__(self, albedo, kd, ks, S, alpha, beta, specular_color):
+        # Lambert BRDF
         self.albedo = albedo
         self.kd = kd
+        
+        # Blinn-Phong BRDF
         self.ks = ks
         self.S = S
-        self.alpha = alpha
+        
+        # Cook-Torrance BRDF
+        self.alpha = alpha # roughness
         self.beta = beta
         self.specular_color = specular_color
 
@@ -30,22 +35,44 @@ class Material:
         return self.kd * self.albedo / math.pi
 
     def blinn_phong(self, n, wi, wo):
-        wh = (wi + wo) / np.linalg.norm(wi + wo, axis=1)
-        return self.ks * (n @ wh.T) ** self.S
+        wh = (wi + wo) / np.linalg.norm(wi + wo, axis=-1, keepdims=True)
+        dot_prod_h = (n[:,:,np.newaxis,:] * wh).sum(axis=-1)
+        dot_prod_h = np.clip(dot_prod_h, 0, 1)
+        
+        return self.ks * (dot_prod_h) ** self.S
 
     def microfacet(self, n, wi, wo):
-        wh = (wi + wo) / np.linalg.norm(wi + wo, axis=1)
+        eps = 1e-16
+        wh = (wi + wo) / np.linalg.norm(wi + wo, axis=-1, keepdims=True)
+        
+        # calculations of dot products. We project negative values to 0
+        dot_prod_h = (n[:,:,np.newaxis,:] * wh).sum(axis=-1)
+        dot_prod_h = np.clip(dot_prod_h, 0, 1)
+        dot_prod_i = (n[:,:,np.newaxis,:] * wi).sum(axis=-1)
+        dot_prod_i = np.clip(dot_prod_i, 0, 1)
+        dot_prod_o = (n[:,:,np.newaxis,:] * wo).sum(axis=-1)
+        dot_prod_o = np.clip(dot_prod_o, 0, 1)
+        dot_prod_i_h = (wo * wi).sum(axis=-1)
+        dot_prod_i_h = np.clip(dot_prod_i_h, 0, 1)
 
-        D = self.alpha ** 2 / (math.pi * ((n @ wh.T) ** 2 * (self.alpha ** 2 - 1) + 1) ** 2)
+        # GGX distribution
+        D = self.alpha ** 2 / (math.pi * ((dot_prod_h) ** 2 * (self.alpha ** 2 - 1) + 1) ** 2)
 
+        # Geometrical term; Schlick approximation of the Smith model
         k = (math.sqrt(self.alpha) + 1) ** 2 / 8
-        G_i = (n @ wi.T) / ((n @ wi.T) * (1 - k) + k)
-        G_o = (n @ wo.T) / ((n @ wo.T) * (1 - k) + k)
+        G_i = (dot_prod_i) / ((dot_prod_i) * (1 - k) + k)
+        G_o = (dot_prod_o) / ((dot_prod_o) * (1 - k) + k)
         G = G_i * G_o
 
-        F = self.specular_color + (1 - self.specular_color) * pow(2, (-5.55473 - 6.98316) * wo @ wh.T)
+        # Smith 
+        #F = self.specular_color + (1 - self.specular_color) * pow(2, (-5.55473 * wo @ wh.T - 6.98316) * wo @ wh.T)
+        
+        # spheical gaussian variant of the Schlick Fresnel approximation
+        F = self.specular_color + (1 - self.specular_color) * (1 - np.maximum(np.zeros((dot_prod_i_h[:,:,:,np.newaxis].shape)), dot_prod_i_h[:,:,:,np.newaxis]))**5
+        
+        f = (D[:,:,:,None] * F * G[:,:,:,None]) / (4 * (dot_prod_i[:,:,:,None]) * (dot_prod_o[:,:,:,None]) + eps)
 
-        return (D * F * G) / (4 * (n @ wi.T) * (n @ wo.T))
+        return f
 
 
 class LightSource:
@@ -55,81 +82,159 @@ class LightSource:
         self.intensity = intensity
 
 
-def shade(img, lightsources, material, kind='lambert'):
+def coor_matrix(img, z=0):
+    nrow, ncol, _ = img.shape
+    mesh_X, mesh_Y = np.meshgrid(np.arange(nrow), np.arange(ncol), indexing='ij')
+    coor = np.stack([mesh_X, mesh_Y], axis=-1)
+    coor = np.append(coor, z * np.ones((nrow, ncol, 1)), axis=-1)
+    return coor
+
+def shade(img, lightsources, material, xo, kind='lambert'):
     # sum_i Li f n.wi
+    positions = coor_matrix(img)
     n = img
-    wi = lightsources.positions
-    wo = np.array([[1, 1, 1]])
+    xi = lightsources.positions
+    
+    wi = xi[np.newaxis, np.newaxis,:,:] - positions[:,:,np.newaxis,:]
+    wi = wi / np.linalg.norm(wi, axis=-1, keepdims=True)
+    wo = xo[np.newaxis, np.newaxis,:,:] - positions[:,:,np.newaxis,:]
+    wo = wo / np.linalg.norm(wo, axis=-1, keepdims=True)
+    
+    dot_prod_i = (n[:,:,np.newaxis,:] * wi).sum(axis=-1)
+    dot_prod_i = np.clip(dot_prod_i, 0, 1)
+    
     if kind == 'lambert':
-        render = np.clip((material.lambert()[None, None, None, :] * (img @ lightsources.positions.T)[:, :, :,
-                                                                    None] * lightsources.colors[None, None, :, :]).sum(
-            axis=2), 0, 1)
+        render = np.clip(
+                (material.lambert()[None, None, None, :] *
+                 (dot_prod_i)[:, :, :,None] *
+                 lightsources.colors[None, None, :, :]
+                 ).sum(axis=2),
+                0, 1)
     elif kind == 'phong':
-        render = np.clip(((material.lambert()[None, None, None, :] + material.blinn_phong(n=n, wi=wi, wo=wo)[:, :, :,
-                                                                     None]) * (img @ lightsources.positions.T)[:, :, :,
-                                                                              None] * lightsources.colors[None, None, :,
-                                                                                      :]).sum(axis=2), 0, 1)
+        render = np.clip((
+                (material.lambert()[None, None, None, :] +
+                 material.blinn_phong(n=n, wi=wi, wo=wo)[:, :, :,None]
+                 ) * (dot_prod_i)[:, :, :,None] * lightsources.colors[None, None, :,:]
+                ).sum(axis=2)
+            , 0, 1)
     elif kind == 'microfacet':
-        render = np.clip((material.microfacet(n=n, wi=wi, wo=wo)[:, :, None, :] * (img @ lightsources.positions.T)[:, :,
-                                                                                  :, None] * lightsources.colors[None,
-                                                                                             None, :, :]).sum(axis=2),
-                         0, 1)
+        render = np.clip((
+                (material.lambert()[None, None, None, :] +
+                 material.microfacet(n=n, wi=wi, wo=wo)
+                 ) * (dot_prod_i)[:, :,:, None] * lightsources.colors[None,None, :, :]
+                ).sum(axis=2),
+                0, 1)
     else:
         raise ValueError()
     return render
 
+def import_and_clean(filename="normal.png"):
+    normalImage = Image.open(filename)
+
+    img = np.array(normalImage)[:, :, :-1]
+    
+    norms = np.linalg.norm(img, axis=-1, keepdims=True)
+    mask = (norms <= 10).squeeze()
+    img[mask] = 1
+    img = img / np.linalg.norm(img, axis=-1, keepdims=True)
+    img[mask] = 0
+    plt.imshow(img)
+    print(normalImage)
+    return img
+
+def save(render, filename):
+    render_img = Image.fromarray((render * 255).astype(np.uint8))
+    render_img.save(filename)
+    
+def hist(img):
+    x1,x2,_ = img.shape
+    img = img.reshape((x1*x2,3))
+    plt.hist(img)
+    plt.show()
 
 if __name__ == '__main__':
     material = Material(np.array([0.75, 0.9, 0.6]), kd=1, ks=1, S=1, alpha=1, beta=1,
                         specular_color=np.array([1, 0, 0]))
+    
+    xo = np.array([[100, 350, 1000]]) # location of camera along Z axis
+    xo = np.array([[0,1,1]])
 
-    if True:
-        lightsources = LightSource(np.array([[0, 3, -1]]), np.array([[1, 1, 1]]), np.ones(1))
+    if False:
+        material = Material(np.array([0.75, 0.9, 0.6]), kd=math.pi, ks=1, S=1, alpha=0.5, beta=1,
+                        specular_color=np.array([0.05,0.54,0.05]))
+        
+        lightsources = LightSource(np.array([[0,1,1000]]), np.array([[1, 1, 1]]), np.ones(1))
 
-        normalImage = Image.open("normal.png")
-        img = np.array(normalImage)[:, :, :-1] / 255
-        print(normalImage)
-        render = shade(img, lightsources, material)
+        img = import_and_clean("normal.png")
+        
+        render = shade(img, lightsources, material, xo)
         plt.imshow(render)
 
-        render_img = Image.fromarray((render * 255).astype(np.uint8))
-        render_img.save("render_lambert_1.png")
+        #save(render, "render_lambert_1.png")
 
-    if True:
+    if False:
+        material = Material(np.array([0.75, 0.9, 0.6]), kd=math.pi, ks=1, S=1, alpha=0.5, beta=1,
+                        specular_color=np.array([0.05,0.54,0.05]))
+        
+        lightsources = LightSource(np.array([[1000, -100, 10], [200, -100, 1000]]),
+                                   np.array([[1, 0, 0], [0, 1, 1]]),
+                                   np.ones(2))
+        
+        lightsources = LightSource(np.array([[1000, -100, 10]]), np.array([[1,0,0]]), np.ones(1))
+        lightsources = LightSource(np.array([[800, 1000, -10]]), np.array([[0,0,1]]), np.ones(1))
+        lightsources = LightSource(np.array([[0,1000,-1]]), np.array([[0,0,1]]), np.ones(1))
+
+        img = import_and_clean("normal.png")
+        
+        render = shade(img, lightsources, material, xo)
+        plt.imshow(render)
+
+        # save(render, "render_lambert_3.png")
+        
+
+    if False:
+        material = Material(np.array([0.75, 0.9, 0.6]), kd=math.pi, ks=1, S=1, alpha=0.5, beta=1,
+                        specular_color=np.array([0.05,0.54,0.05]))
+        lightsources = LightSource(np.array([[0,1,1000]]), np.array([[1,1,1]]), np.ones(1))
+
+        img = import_and_clean("normal.png")
+        
+        render = shade(img, lightsources, material, xo, kind='phong')
+        plt.imshow(render)
+        
+        #save(render, "render_phong_1.png")
+
+
+    if False:
+        material = Material(np.array([0.75, 0.9, 0.6]), kd=math.pi, ks=1, S=1, alpha=0.5, beta=1,
+                        specular_color=np.array([1,1,1]))
+        
+        lightsources = LightSource(np.array([[0,1,1000]]), np.array([[1,1,1]]), np.ones(1))
+        
+        img = import_and_clean(filename="normal.png")
+
+        render = shade(img, lightsources, material, xo, kind='microfacet')
+        plt.imshow(render)
+        
+        #save(render, "render_microfacet_3.png")
+        
+    if False:
+        material = Material(np.array([0.75, 0.9, 0.6]), kd=1, ks=1, S=1, alpha=1, beta=1,
+                        specular_color=np.array([0.05,0.05,0.05]))
+        
         lightsources = LightSource(np.array([[0, 3, -1], [3, -1, 0], [-1, 0, 3]]),
                                    np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]]),
                                    np.ones(3))
+        
+        img = import_and_clean(filename="normal.png")
+        
+        alphas = [1,5,10,50,100]
+        for alpha in alphas:
+            material = Material(np.array([0.75, 0.9, 0.6]), kd=1, ks=1, S=1, alpha=alpha, beta=1,
+                        specular_color=np.array([0.05,0.05,0.05]))
+            render = shade(img, lightsources, material, xo, kind='microfacet')
+            plt.clf()
+            plt.imshow(render)
+            
+            save(render, "render_microfacet_3_{}.png".format(alpha))
 
-        normalImage = Image.open("normal.png")
-        img = np.array(normalImage)[:, :, :-1] / 255
-        print(normalImage)
-        render = shade(img, lightsources, material)
-        plt.imshow(render)
-
-        render_img = Image.fromarray((render * 255).astype(np.uint8))
-        render_img.save("render_lambert_3.png")
-
-    if True:
-        lightsources = LightSource(np.array([[0, 3, -1]]), np.array([[1, 1, 1]]), np.ones(1))
-
-        normalImage = Image.open("normal.png")
-        img = np.array(normalImage)[:, :, :-1] / 255
-        print(normalImage)
-        render = shade(img, lightsources, material, kind='phong')
-        plt.imshow(render)
-
-        render_img = Image.fromarray((render * 255).astype(np.uint8))
-        render_img.save("render_phong_1.png")
-
-    if True:
-        lightsources = LightSource(np.array([[0, 3, -1], [3, -1, 0], [-1, 0, 3]]),
-                                   np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]]),
-                                   np.ones(3))
-        normalImage = Image.open("normal.png")
-        img = np.array(normalImage)[:, :, :-1] / 255
-
-        render = shade(img, lightsources, material, kind='microfacet')
-        plt.imshow(render)
-
-        render_img = Image.fromarray((render * 255).astype(np.uint8))
-        render_img.save("render_microfacet_3.png")
