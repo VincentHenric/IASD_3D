699700f4358c39d87b2f1aa765388d5e599b904c
HENRIC <vincent.henric@hotmail.fr>
2020-04-03 11:39:29 +0200

    add code

--- project/code/clean_data.py
+++ project/code/clean_data.py
@@ -8,6 +8,7 @@ Created on Tue Mar 24 10:55:06 2020
 
 import os
 
+from utils.ply import write_ply, read_ply
 import numpy as np
 
 PATH = '../data'
@@ -20,15 +21,107 @@ CLASSES = {1: 'man-made terrain',
            7: 'scanning artefacts',
            8: 'cars'}
 
+ZOOM_COORDINATES = {
+        'bildstein_station1_xyz_intensity_rgb':{'min_x':12, 'max_x':55, 'min_y':-10, 'max_y':40},
+        'bildstein_station3_xyz_intensity_rgb':{'min_x':-20, 'max_x':17, 'min_y':-70, 'max_y':-25},
+        'bildstein_station5_xyz_intensity_rgb':{'min_x':-40, 'max_x':-5, 'min_y':-35, 'max_y':15}
+        }
+
+REF_POINTS = [{'bildstein_station1_xyz_intensity_rgb':{'x':30.249, 'y':32.927, 'z':31.1},
+               'bildstein_station3_xyz_intensity_rgb':{'x':3.597, 'y':-65.583, 'z':28.228},
+               'bildstein_station5_xyz_intensity_rgb':{'x':-30.251, 'y':3.979, 'z':32.137}},
+              {'bildstein_station1_xyz_intensity_rgb':{'x':46.177, 'y':26.846, 'z':30.41},
+               'bildstein_station3_xyz_intensity_rgb':{'x':-13.306, 'y':-62.783, 'z':28.636},
+               'bildstein_station5_xyz_intensity_rgb':{'x':-14.469, 'y':-2.091, 'z':32.137}}
+]
+#np.array([[3.16, 4.73, 1.12],
+#        [24.31, 51.87, -3.54],
+#        [47.4, 63.9, -1.15],
+#        [48.6, -27.3, 6.46]])
+    
 def load_dataset(filename, max_rows=None):
     data = np.loadtxt(os.path.join(PATH, filename), max_rows=max_rows)
     return data
     #return data[:,:3], data[:,[3]], data[:,4:]
 
-def load_labels(filename, max_rows=None):
-    labels = np.loadtxt(os.path.join(PATH, filename), max_rows=max_rows)
-    return labels
+def save_dataset_npy(data, filename_rad):
+    np.save(os.path.join(PATH, '{}_transformed.npy'.format(filename_rad)), data)
+    #np.savetxt(os.path.join(PATH, '{}_transformed.txt'.format(filename_rad)), data)
 
-data = load_dataset('bildstein_station1_xyz_intensity_rgb.txt', max_rows = 100)
-labels = load_labels('bildstein_station1_xyz_intensity_rgb.labels', max_rows = 100)
+def save_dataset_ply(data, filename_rad, suffix='transformed'):
+    if suffix!='':
+        suffix = '_' + suffix
+    write_ply(os.path.join(PATH, '{}{}.ply'.format(filename_rad, suffix)),
+              data,
+              ['x', 'y', 'z', 'intensity', 'red', 'green', 'blue'])
+
+def normalize_intensities(intensities, low=-2048, high=2047):
+    intensities = (intensities - low)/(high-low)*255
+    return intensities
+
+def clean_dataset(filename_rad, max_rows=None):
+    data = load_dataset(filename_rad+'.txt', max_rows)
+    labels = load_dataset(filename_rad+'.labels', max_rows)
+    data = np.concatenate((data, labels[:,None]), axis=1)
+    data = data[data[:,-1]==5]
+    data = np.delete(data, -1, axis=1)
+    save_dataset_ply(data, filename_rad)
+    
+def select_points(filename_rad):
+    cloud_ply = read_ply(os.path.join(PATH, filename_rad+'_transformed.ply'))
+    
+    for coord in ['x', 'y', 'z']:
+        min_val = ZOOM_COORDINATES[filename_rad].get('min_'+coord)
+        if min_val:
+            cloud_ply = cloud_ply[cloud_ply[coord]>=min_val]
+            
+        max_val = ZOOM_COORDINATES[filename_rad].get('max_'+coord)
+        if min_val:
+            cloud_ply = cloud_ply[cloud_ply[coord]<=max_val]
+            
+    return cloud_ply
+    
+def center(cloud_points):
+    return cloud_points - cloud_points.mean(axis=0)
+
+if __name__ == '__main__':
+    if False:
+        filename_rad = 'bildstein_station5_xyz_intensity_rgb'
+        clean_dataset(filename_rad)
+        
+    if True:
+        filename_rad = 'bildstein_station1_xyz_intensity_rgb'
+        
+        # selection
+        cloud_ply = select_points(filename_rad)
+        
+        cloud_points = np.vstack((cloud_ply['x'], cloud_ply['y'], cloud_ply['z'])).T
+        colors = np.vstack((cloud_ply['red'], cloud_ply['green'], cloud_ply['blue'])).T
+        intensities = cloud_ply['intensity']
+        
+        # processing
+        #cloud_points = center(cloud_points)
+        intensities = normalize_intensities(intensities, low=-2048, high=2047)
+        
+        save_dataset_ply((cloud_points, intensities[:,None].astype('uint8'), colors.astype('uint8')),
+                       filename_rad,
+                       suffix='cutted')
+        
+
+
+#filename_rad = 'bildstein_station1_xyz_intensity_rgb'
+#max_rows=None
+#
+#center = np.array([[19.823, 13.171, 5.221]])
+#data_min = np.array([[-104.7  ,  -133.527,   -17.109]])
+#data_max = np.array([[120.578,  112.858,   47.372]])
+#box_dim = np.array([[225.278, 246.385, 64.481]])
+#
+#coordinates_lim = np.array([[3.16, 4.73, 1.12],
+#                            [24.31, 51.87, -3.54],
+#                            [47.4, 63.9, -1.15],
+#                            [48.6, -27.3, 6.46]])
+#
+#data = load_dataset('bildstein_station1_xyz_intensity_rgb.txt', max_rows = None)
+#labels = load_labels('bildstein_station1_xyz_intensity_rgb.labels', max_rows = 100)
     
--- project/code/features.py
+++ project/code/features.py
@@ -14,6 +14,7 @@ from utils.ply import write_ply, read_ply
 import time
 import os
 
+import subsampling
 
 def local_PCA(points):
     
@@ -46,8 +47,8 @@ def neighborhood_PCA(query_points, cloud_points, radius):
     
     return all_eigenvalues, all_eigenvectors
 
-def get_general_features(cloud_points, colors, intensities, radius):
-    n = len(cloud_points)
+def get_general_features(query_points_indices, cloud_points, colors, intensities, radius):
+    n = len(query_points_indices)
     epsilon = 1e-16
     
     # prepare arrays
@@ -59,7 +60,7 @@ def get_general_features(cloud_points, colors, intensities, radius):
     # This function needs to compute PCA on the neighborhoods of all query_points in cloud_points
     kdtree = KDTree(cloud_points)  
     
-    neighborhood_indices = kdtree.query_radius(cloud_points, r=radius, count_only=False, return_distance=False)
+    neighborhood_indices = kdtree.query_radius(cloud_points[query_points_indices], r=radius, count_only=False, return_distance=False)
     
     for i, neighbor_index_list in enumerate(neighborhood_indices):
         neighborhoods = cloud_points[neighbor_index_list,:]
@@ -73,7 +74,6 @@ def get_general_features(cloud_points, colors, intensities, radius):
     all_geometric[:,2] = all_eigenvalues[:,0]/(all_eigenvalues[:,2]+epsilon)
     
     return all_normals, all_geometric, all_intensities, neighborhood_indices
-    
 
 def compute_features(query_points_indices, cloud_points, colors, intensities, radius):
     epsilon = 1e-16
@@ -82,11 +82,11 @@ def compute_features(query_points_indices, cloud_points, colors, intensities, ra
     query_cov = np.zeros((len(query_points_indices), 10, 10))
     
     # preprocessing for colors
-    if colors.dtype in [np.uint8, 'uint8']:
+    if colors.dtype in [np.uint8, 'uint8'] or colors.max()>1:
         colors = colors/255
     
     # Compute the features for all query points in the cloud
-    all_normals, all_geometric, all_intensities, neighborhood_indices = get_general_features(cloud_points, colors, intensities, radius)
+    all_normals, all_geometric, all_intensities, neighborhood_indices = get_general_features(np.arange(len(cloud_points)), cloud_points, colors, intensities, radius)
     
     # take indices for query points
     neighborhood_indices = neighborhood_indices[query_points_indices]
@@ -108,13 +108,13 @@ def compute_features(query_points_indices, cloud_points, colors, intensities, ra
         alphas = pi_p_vectors @ all_normals[query_point_index].T
         betas = (pi_p_vectors * all_normals[neighbor_index_list]).sum(axis=1)
         gammas = all_normals[neighbor_index_list] @ all_normals[query_point_index].T
-        intensities = all_intensities[neighbor_index_list]
+        ent_intensities = all_intensities[neighbor_index_list].copy()
         
         # normalization
         alphas = (alphas-alphas.min())/(alphas.max()-alphas.min()+epsilon)
         betas = (betas-betas.min())/(betas.max()-betas.min()+epsilon)
         gammas = (gammas-gammas.min())/(gammas.max()-gammas.min()+epsilon)
-        intensities = (alphas-alphas.min())/(alphas.max()-alphas.min()+epsilon)
+        ent_intensities = (ent_intensities-ent_intensities.min())/(ent_intensities.max()-ent_intensities.min()+epsilon)
 
         # concatenation
         features = np.concatenate((all_geometric[neighbor_index_list],
@@ -122,7 +122,7 @@ def compute_features(query_points_indices, cloud_points, colors, intensities, ra
                                    betas[:,None],
                                    gammas[:,None],
                                    colors[neighbor_index_list],
-                                   intensities[:,None]), axis=1)
+                                   ent_intensities), axis=1)
         
         bary = features.mean(axis=0, keepdims=True)
         Q = features - bary
@@ -132,10 +132,109 @@ def compute_features(query_points_indices, cloud_points, colors, intensities, ra
 
     return query_cov
 
+
+
+
+#def compute_features_2(query_points_indices, cloud_points, colors, intensities, radius):
+#    epsilon = 1e-16
+#
+#    # Initialize covariance matrices
+#    query_cov = np.zeros((len(query_points_indices), 10, 10))
+#    
+#    # preprocessing for colors
+#    if colors.dtype in [np.uint8, 'uint8'] or colors.max()>1:
+#        colors = colors/255
+#    
+#    
+#    kdtree = KDTree(cloud_points)  
+#    
+#    # take indices for query points
+#    neighborhood_indices = kdtree.query_radius(cloud_points[query_points_indices], r=radius, count_only=False, return_distance=False)
+#    all_neighborhood_indices, all_neighborhood_local_indices = np.unique(np.concatenate(neighborhood_indices), return_index=True)
+#
+#    
+#    # compute remaining features and 
+#    for i, neighbor_index_list in enumerate(neighborhood_indices):
+#        query_point_index = query_points_indices[i]
+#        all_normals, all_geometric, all_intensities, _ = get_general_features(all_neighborhood_indices, cloud_points, colors, intensities, radius)
+#        
+#        pi_p_vectors = cloud_points[neighbor_index_list]-cloud_points[query_point_index]
+#        pi_p_norm = np.linalg.norm(pi_p_vectors, axis=1, keepdims=True)
+#        indices_to_remove = np.where(pi_p_norm==0)[0]
+#        neighbor_index_list = np.delete(neighbor_index_list, indices_to_remove)
+#        
+#        pi_p_vectors = pi_p_vectors[pi_p_norm[:,0]!=0,:] / pi_p_norm[pi_p_norm[:,0]!=0]
+#        
+#        if len(neighbor_index_list)<2:
+#            continue
+#        
+#        # get remaining features
+#        alphas = pi_p_vectors @ all_normals[query_point_index].T
+#        betas = (pi_p_vectors * all_normals[neighbor_index_list]).sum(axis=1)
+#        gammas = all_normals[neighbor_index_list] @ all_normals[query_point_index].T
+#        intensities = all_intensities[neighbor_index_list]
+#        
+#        # normalization
+#        alphas = (alphas-alphas.min())/(alphas.max()-alphas.min()+epsilon)
+#        betas = (betas-betas.min())/(betas.max()-betas.min()+epsilon)
+#        gammas = (gammas-gammas.min())/(gammas.max()-gammas.min()+epsilon)
+#        intensities = (intensities-intensities.min())/(intensities.max()-intensities.min()+epsilon)
+#
+#        # concatenation
+#        features = np.concatenate((all_geometric[neighbor_index_list],
+#                                   alphas[:,None],
+#                                   betas[:,None],
+#                                   gammas[:,None],
+#                                   colors[neighbor_index_list],
+#                                   intensities), axis=1)
+#        
+#        bary = features.mean(axis=0, keepdims=True)
+#        Q = features - bary
+#        cov = 1/(len(features)-1) * (Q.T @ Q)
+#        
+#        query_cov[i,:] = cov
+#
+#    return query_cov
+
+def add_noise(cloud_points, sigma=0.001):
+    noise = np.random.normal(size=(len(cloud_points),3), scale=sigma)
+    return cloud_points + noise
+
+def select_top_indices(query_cov, q=0.95):
+    dets = np.abs(np.linalg.det(query_cov))
+    quantile = np.quantile(dets, q)
+    return np.where(dets>quantile)[0]
+
+def compute_r_bar(cloud_points):
+    kdtree = KDTree(cloud_points)  
+    dist, ind = kdtree.query(cloud_points, k=2, return_distance=True)
+
 def compute_entropy(intensities, **kwargs):
     hist = np.histogram(intensities, density=True, **kwargs)[0]
     return entropy(hist)
 
+def identify_good_radius(cloud_points, radiuses=[0.1, 0.3, 0.5, 1, 5], N=100000, **kwargs):
+    kdtree = KDTree(cloud_points)  
+
+    indices = np.random.randint(len(cloud_points), size=N)  
+    
+    sizes = {}
+    for radius in radiuses:
+        size = kdtree.query_radius(cloud_points[indices,:], r=radius, count_only=True, return_distance=False)
+        lim = np.quantile(size, 0.95)
+        low_lim = np.quantile(size, 0.05)
+        plt.clf
+        plt.hist(size, range=(0,lim), **kwargs)
+        plt.axvline(low_lim, color='red')
+        plt.text(low_lim,-3,low_lim, color='red')
+        #plt.hist(size, range=(0,200))
+        plt.show()
+        sizes[radius] = size
+    return sizes
+
+def normalize_intensities(intensities, low=-2048, high=2047):
+    intensities = (intensities - low)/(high-low)*255
+    return intensities
 
 # ------------------------------------------------------------------------------------------
 #
@@ -151,28 +250,56 @@ if __name__ == '__main__':
     data_path = '../data'
     saved_data_path = '../saved_data'
 
-    # Uniform sampling to find interesting points
+    # Find a good radius for neighbors
     # ********************
     #
 
-    if True:
+    if False:
 
         # Load cloud as a [N x 3] matrix
         filename = 'bunny_original.ply'
-        filename = 'Lille_street_small.ply'
+        #filename = 'Lille_street_small.ply'
+        #filename = 'bildstein_station1_xyz_intensity_rgb_cutted.ply'
         name = filename.split('.')[0]
         cloud_path = os.path.join(data_path, filename)
         cloud_ply = read_ply(cloud_path)
         
         cloud_points = np.vstack((cloud_ply['x'], cloud_ply['y'], cloud_ply['z'])).T
-        colors = np.zeros((len(cloud_points),3))
-        intensities = np.ones((len(cloud_points),1))
-        
-        colors = np.vstack((cloud_ply['R'], cloud_ply['G'], cloud_ply['B'])).T
-        intensities = cloud_ply['intensity']
+
 
         # Parameters
-        radius = 0.3
+        radiuses = [0.1, 0.3, 0.5, 1, 5]
+        
+        radiuses = [0.01, 0.02, 0.03]
+
+        # plot the number of neighbors
+        sizes = identify_good_radius(cloud_points, radiuses=radiuses, N=100000, bins=100)
+        
+
+    # Uniform sampling to find interesting points
+    # ********************
+    #
+
+    if False:
+
+        # Load cloud as a [N x 3] matrix
+        filename = 'bunny_original.ply'
+        #filename = 'Lille_street_small.ply'
+        #filename = 'bildstein_station1_xyz_intensity_rgb_cutted.ply'
+        name = filename.split('.')[0]
+        cloud_path = os.path.join(data_path, filename)
+        cloud_ply = read_ply(cloud_path)
+        
+        cloud_points = np.vstack((cloud_ply['x'], cloud_ply['y'], cloud_ply['z'])).T
+        #colors = np.zeros((len(cloud_points),3))
+        #intensities = np.ones((len(cloud_points),1))
+        
+        colors = np.vstack((cloud_ply['red'], cloud_ply['green'], cloud_ply['blue'])).T
+        intensities = cloud_ply['intensity']
+        
+
+        # Parameters
+        radius = 0.02
         quantile = 0.9
         percent = 0.1
         sampling_indices = np.random.randint(len(cloud_points), size=round(percent*len(cloud_points)))
@@ -208,28 +335,84 @@ if __name__ == '__main__':
     if False:
 
         # Load cloud as a [N x 3] matrix
+        filename = 'bildstein_station1_xyz_intensity_rgb_cutted.ply'
         filename = 'bunny_original.ply'
         name = filename.split('.')[0]
         cloud_path = os.path.join(data_path, filename)
         cloud_ply = read_ply(cloud_path)
         
         cloud_points = np.vstack((cloud_ply['x'], cloud_ply['y'], cloud_ply['z'])).T
-        colors = np.vstack((cloud_ply['R'], cloud_ply['G'], cloud_ply['B'])).T
+        colors = np.zeros((len(cloud_points),3))
+        intensities = np.ones((len(cloud_points),1))
+        
+        colors = np.vstack((cloud_ply['red'], cloud_ply['green'], cloud_ply['blue'])).T
         intensities = cloud_ply['intensity']
+        
         
         # Load points of interest
         query_points_indices = np.load(os.path.join(data_path, 'query_indices_{}.npy'.format(name)))
-
+        query_points_indices = np.arange(len(cloud_points))
+        
         # Parameters
         radius = 0.3
+        radius = 0.02
+        noise = 0.001
         query_points_indices = np.array([1,10,100])
+        
+        # Preprocessing
+        cloud_points = add_noise(cloud_points, noise)
         
         # Computations
         query_cov = compute_features(query_points_indices, cloud_points, colors, intensities, radius)
         
         # Save results
-        np.save(os.path.join(saved_data_path, 'covariance_{}.npy'.format(name)), query_cov)
+        np.save(os.path.join(saved_data_path, 'covariance_{}_radius-{}_noise-{}.npy'.format(name, radius, noise)), query_cov)
         
         #write_ply('../data/Lille_street_small_featured_{}.ply'.format(radius), [new_cloud], ['x', 'y', 'z', 'verticality', 'linearity', 'planarity', 'sphericity'])
 
 
+    # Features analysis
+    # ********************
+    #
+
+    if True:
+
+        # Load cloud as a [N x 3] matrix
+        filename = 'bildstein_station3_xyz_intensity_rgb_cutted.ply'
+        radius=0.3
+        noise=0.001
+        nb_values = 10
+        noise_2 = 2
+        name = filename.split('.')[0]
+        cloud_path = os.path.join(data_path, filename)
+        cloud_ply = read_ply(cloud_path)
+        #covariances = np.load(os.path.join(saved_data_path, 'covariance_{}_radius-{}_noise-{}.npy'.format(name, radius, noise)))
+        covariances = np.load(os.path.join(saved_data_path, 'covariance_{}_radius-{}_noise-{}-{}-{}.npy'.format(name, radius, noise, nb_values, noise_2)))
+
+        
+        cloud_points = np.vstack((cloud_ply['x'], cloud_ply['y'], cloud_ply['z'])).T
+        colors = np.vstack((cloud_ply['red'], cloud_ply['green'], cloud_ply['blue'])).T
+        intensities = cloud_ply['intensity']
+        
+        
+        # analysis
+        #determinants = np.linalg.det(covariances)
+        
+        #plt.hist(determinants[:100], bins=50)
+        
+        #indices = select_top_indices(query_cov[:,:6,:6], q=0.95)
+        indices = select_top_indices(covariances, q=0.95)
+        
+        sailant = np.zeros((len(cloud_points),1))
+        sailant[indices] = 1
+  
+#        write_ply(os.path.join(saved_data_path, 'sailant_{}_radius-{}_noise-{}.ply'.format(name, radius, noise)),
+#                  (cloud_points, intensities[:,None], colors, sailant),
+#                  ['x', 'y', 'z', 'intensity', 'red', 'green', 'blue', 'sailant'])
+        
+        write_ply(os.path.join(saved_data_path, 'sailant_{}_radius-{}_noise-{}-{}-{}.ply'.format(name, radius, noise, nb_values, noise_2)),
+                  (cloud_points, intensities[:,None], colors, sailant),
+                  ['x', 'y', 'z', 'intensity', 'red', 'green', 'blue', 'sailant'])
+
+        
+        
